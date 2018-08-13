545a0dbbc2c687cc99e85260cbe4a93340448529
Andrea D'Incecco <andrea.dincecco@gmail.com>
2018-08-13 11:51:51 +0200

    visualizzazione dati da json statico

--- src/app/previsioni-list/previsioni-list.component.ts
+++ src/app/previsioni-list/previsioni-list.component.ts
@@ -6,8 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./previsioni-list.component.css']
 })
 export class PrevisioniListComponent implements OnInit {
-  title = 'Previsioni del tempo';
-
+  title = 'Previsioni meteo';
+  json = {
+            "coord": {
+                "lon": 7.68,
+                "lat": 45.07
+            },
+            "weather": [
+                {
+                    "id": 802,
+                    "main": "Clouds",
+                    "description": "nubi sparse",
+                    "icon": "03d"
+                }
+            ],
+            "base": "stations",
+            "main": {
+                "temp": 22.74,
+                "pressure": 1014,
+                "humidity": 78,
+                "temp_min": 22,
+                "temp_max": 24
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 1.5,
+                "deg": 100
+            },
+            "clouds": {
+                "all": 40
+            },
+            "dt": 1534148400,
+            "sys": {
+                "type": 1,
+                "id": 5795,
+                "message": 0.0193,
+                "country": "IT",
+                "sunrise": 1534134519,
+                "sunset": 1534185523
+            },
+            "id": 3165524,
+            "name": "Turin",
+            "cod": 200
+        };
   constructor() { }
 
   ngOnInit() {
